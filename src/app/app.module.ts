602a216235f70e6a61442a4c489aae6bd8f7eac2
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-14 07:57:20 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { SideBarComponent } from './Components/side-bar/side-bar.component';
+import { HousesComponent } from './Components/houses/houses.component';
+import { ReservationsComponent } from './Components/reservations/reservations.component';
+import { UsersComponent } from './Components/users/users.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SideBarComponent],
+  declarations: [AppComponent, HeaderComponent, SideBarComponent, HousesComponent, ReservationsComponent, UsersComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
