990bf441d45ffeaf824bfcbffe4548b46bfddf44
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-14 07:01:10 +0300

    update

---
+++ src/app/Models/IHouse.ts
@@ -0,0 +1,16 @@
+export interface IHouse {
+  _id?: string;
+  name: string;
+  address: string;
+  price: number;
+  currencySymbol?: string;
+  numberOfRooms?: number;
+  numberOfBedrooms: number;
+  guestNumber: number;
+  rate?: number;
+  description: string;
+  comments?: string;
+  images: string[];
+  category: string;
+  userId: string;
+}
