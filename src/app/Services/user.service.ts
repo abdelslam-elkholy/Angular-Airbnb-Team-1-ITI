56ae780a8ee5b0e1671d2cc871bd5662c1472a1e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-16 20:00:54 +0300

    update

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  login(email: string, password: string): Observable<object> {
+    return this.httpClient.post(`${environment.BaseApiURL}/user//signin`, {
+      email,
+      password,
+    });
+  }
+
+  getUsers() {
+    return this.httpClient.get(`${environment.BaseApiURL}/user/`);
+  }
+
+  getUser(id: number) {
+    return this.httpClient.get(`${environment.BaseApiURL}/user/${id}`);
+  }
+
+  deleteUser(id: number) {
+    return this.httpClient.delete(`${environment.BaseApiURL}/user/${id}`);
+  }
 }
