b2ff3136621faa3cfc532d2cea6d486b90de0e99
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-19 08:06:37 +0300

    update

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -10,21 +10,49 @@ import { UserService } from 'src/app/Services/user.service';
 })
 export class UsersComponent implements OnInit {
   users: IUser[] = [];
+  deactivatedUsers: IUser[] = [];
+  activeUsers: boolean = true;
 
   constructor(private userService: UserService, router: Router) {}
 
   ngOnInit(): void {
+    this.getAllUsers();
+    this.getDeactivatedUsers();
+  }
+
+  getAllUsers() {
     this.userService.getAllUsers().subscribe((users) => {
       this.users = users;
+      console.log(this.users);
+    });
+  }
+
+  getDeactivatedUsers() {
+    this.userService.getDeactivatedUsers().subscribe((users) => {
+      this.deactivatedUsers = users;
+      console.log(this.deactivatedUsers);
+    });
+  }
+
+  activateUser(id: string) {
+    this.userService.activatUser(id).subscribe((user) => {
+      this.getAllUsers();
+      this.getDeactivatedUsers();
+    });
+  }
+
+  deactivateUser(id: string) {
+    this.userService.deactivatUser(id).subscribe((user) => {
+      this.getAllUsers();
+      this.getDeactivatedUsers();
     });
   }
 
   deleteUser(id: string) {
     if (confirm('Are you sure you want to delete this user?')) {
       this.userService.deleteUser(id).subscribe((user) => {
-        this.userService.getAllUsers().subscribe((users) => {
-          this.users = users;
-        });
+        this.getAllUsers();
+        this.getDeactivatedUsers();
       });
     }
   }
