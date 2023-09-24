65ad5b204806dd027d7a3115611adf4fee8c1d71
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-24 08:16:20 +0300

    update

--- src/app/Components/reservations/reservations.component.ts
+++ src/app/Components/reservations/reservations.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ReservationsService } from './../../Services/reservations.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.css']
+  styleUrls: ['./reservations.component.css'],
 })
-export class ReservationsComponent {
-
+export class ReservationsComponent implements OnInit {
+  constructor(private reservationsService: ReservationsService) {}
 }
--- src/app/Models/IReservation.ts
+++ src/app/Models/IReservation.ts
@@ -1,7 +1,7 @@
 export interface IReservation {
   _id: string;
-  userId: string;
-  houseId: string;
+  userId: object;
+  houseId: object;
   checkIn: Date;
   checkOut: Date;
   price: number;
