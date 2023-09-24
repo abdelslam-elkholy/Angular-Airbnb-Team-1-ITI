45432a219d60dc25fc42cc4f3ae004635106247e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-24 12:47:29 +0300

    update

--- src/app/Components/team/team.component.ts
+++ src/app/Components/team/team.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent {
+  members = [
+    {
+      name: 'Abdelslam Elkholy',
+      linkedin: 'https://www.linkedin.com/in/abdelslam-elkholy-1b1b1a1b1/',
+      github: 'https://github.com/abdelslam-elkholy',
+      img: 'abdelslam.jpeg',
+    },
 
+    {
+      name: 'Kariman Abdellatif',
+      linkedin: 'https://www.linkedin.com/in/kariman-abd-elatif-2371b61a3/',
+      github: 'https://github.com/KarimanOmar',
+      img: 'kariman.jpeg',
+    },
+
+    {
+      name: 'Mariam Hesham',
+      linkedin: 'https://www.linkedin.com/in/mariam-hesham-m2352/',
+      github: 'https://github.com/mariamhesham414',
+      img: 'mariam.jpeg',
+    },
+
+    {
+      name: 'Mohamed Abdelrahman',
+      linkedin: 'https://www.linkedin.com/in/mohammed-abdl-rahman-/',
+      github: 'https://github.com/Mohammad-abdo',
+      img: 'mohamed.jpeg',
+    },
+  ];
 }
