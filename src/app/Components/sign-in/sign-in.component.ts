b2ed6b80d2faa2cc3732d8cc577daab2209c1130
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-28 07:22:48 +0300

    update

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
+  email: string = '';
+  password: string = '';
+  constructor(private authService: AuthService) {}
 
+  login(): void {
+    console.log(this.email, this.password);
+    this.authService.login(this.email, this.password).subscribe((data) => {});
+  }
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
@@ -9,17 +9,11 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {}
 
   login(email: string, password: string): Observable<object> {
-    return this.httpClient.post(`${environment.BaseApiURL}/users/signin`, {
-      email,
-      password,
-    });
-  }
-
-  getToken(name: string): string | undefined {
-    const value = '; ' + document.cookie;
-    const parts = value.split('; ' + name + '=');
-
-    if (parts.length === 2) return parts.pop()!.split(';').shift();
-    return undefined;
+    return this.httpClient
+      .post(`${environment.BaseApiURL}/users/signin`, {
+        email,
+        password,
+      })
+      .pipe(map((response: any) => response.token));
   }
 }
